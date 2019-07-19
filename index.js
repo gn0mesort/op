37490ef094069d0a409cc8904d4b3311d137bd7b
Alexander Rothman <gnomesort@megate.ch>
2019-07-19 16:59:59 -0700

    Allow retrieval of internal discord.js library

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ const std = {
 module.exports = {
   Bot: require(std.path.join(__dirname, 'src/Bot.js')),
   Command: require(std.path.join(__dirname, 'src/Command.js')),
-  Plugin: require(std.path.join(__dirname, 'src/Plugin.js'))
+  Plugin: require(std.path.join(__dirname, 'src/Plugin.js')),
+  Discord: require('discord.js')
 };
